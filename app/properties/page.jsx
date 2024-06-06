33be0853dec4a8ef968501785ad8cdd49ad8d855
youness <younessaitmbarek99@gmail.com>
2024-06-06 14:56:47 +0000

    update

---
+++ app/properties/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '@/components/navbar/Navbar';
+
+const Property = () => {
+  return (
+    <>
+        <Navbar/>
+        <div className='bg-red-700 '>Property</div>
+    </>
+  )
+}
+
+export default Property
